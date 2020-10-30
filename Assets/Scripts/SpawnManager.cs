d6d646709c89740442ebd14c0101557323f33476
Dominic Frei <business.dominic.frei@gmail.com>
2020-10-30 03:20:54 +0000

    Spawn loot at the start randomly.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,23 +1,45 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] PlayerSegment playerSegment = default;
+    [SerializeField] PlayerSegment playerSegmentPrefab = default;
+    [SerializeField] Loot lootPrefab = default;
+
+    int playingFieldBoundsLeft = -8;
+    int playingFieldBoundsRight = 8;
+    int playingFieldBoundsUpper = 4;
+    int playingFieldBoundsLower = -4;
 
     void Awake()
     {
-        if (!playerSegment)
+        if (!playerSegmentPrefab)
         {
             Debug.Log("Could not retrieve PlayerSegment.");
         }
     }
 
+    void Start()
+    {
+        int randomX = Random.Range(playingFieldBoundsLeft - 1, playingFieldBoundsRight + 1);
+        int randomY = Random.Range(playingFieldBoundsLower - 1, playingFieldBoundsUpper + 1);
+        Vector3 lootSpawnPosition = new Vector3(randomX, randomY, 0.0f);
+        Instantiate(lootPrefab, lootSpawnPosition, Quaternion.identity);
+    }
+
     public GameObject AddPlayerSegment(GameObject objectToFollow)
     {
+        // Spawn a new player segment.
         Vector3 spawnLocation = objectToFollow.transform.position;
         Vector3 roundedSpawnLocation = new Vector3(Mathf.Round(spawnLocation.x), Mathf.Round(spawnLocation.y), Mathf.Round(spawnLocation.z));
-        PlayerSegment playerSegmentInstance = Instantiate(playerSegment, roundedSpawnLocation, Quaternion.identity);
+        PlayerSegment playerSegmentInstance = Instantiate(playerSegmentPrefab, roundedSpawnLocation, Quaternion.identity);
         playerSegmentInstance.Instantiate(objectToFollow);
+
+        // Spawn a new loot object.
+
+
         return playerSegmentInstance.gameObject;
     }
+
 }
