12f625c17587cf025dc32c9ca2c2e302e2bf2b55
Dominic Frei <business.dominic.frei@gmail.com>
2020-10-31 01:58:43 +0000

    Rewrite the way loot spawns by creating a map instead of continuously trying random positions.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SocialPlatforms;
 
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] PlayerSegment playerSegmentPrefab = default;
     [SerializeField] Loot lootPrefab = default;
 
-    int playingFieldBoundsLeft = -8;
-    int playingFieldBoundsRight = 8;
-    int playingFieldBoundsUpper = 4;
-    int playingFieldBoundsLower = -4;
+    int playingFieldBoundsLeft = 0;
+    int playingFieldBoundsRight = 16;
+    int playingFieldBoundsUpper = 8;
+    int playingFieldBoundsLower = 0;
 
     void Awake()
     {
@@ -42,11 +41,43 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnLoot()
     {
-        int randomX = Random.Range(playingFieldBoundsLeft, playingFieldBoundsRight + 1);
-        int randomY = Random.Range(playingFieldBoundsLower, playingFieldBoundsUpper + 1);
-        Vector3 lootSpawnPosition = new Vector3(randomX, randomY, 0.0f);
-        Debug.Log("Spawning loot at: " + lootSpawnPosition);
-        _ = Instantiate(lootPrefab, lootSpawnPosition, Quaternion.identity);
+        List<Vector3> availablePositions = GenerateMapWithUnoccupiedPostions();
+        if (availablePositions.Count != 0)
+        {
+            int randomLocationIndex = UnityEngine.Random.Range(0, availablePositions.Count);
+            Vector3 lootSpawnPosition = availablePositions[randomLocationIndex];
+            Debug.Log("Spawning loot at: " + lootSpawnPosition);
+            _ = Instantiate(lootPrefab, lootSpawnPosition, Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("No unoccupied positions left. Player won.");
+        }
+    }
+
+    // Creates a new map (2-dimensional array) with the playing field which will be used to check
+    // if a position is already used or not in order to spawn loot only at unoccupied positions.
+    List<Vector3> GenerateMapWithUnoccupiedPostions()
+    {
+        int width = playingFieldBoundsRight - playingFieldBoundsLeft + 1;
+        int height = playingFieldBoundsUpper - playingFieldBoundsLower + 1;
+
+        List<Vector3> availablePositions = new List<Vector3>();
+
+        for (int i = 0; i < width + 1; i++)
+        {
+            for (int j = 0; j < height + 1; j++)
+            {
+                Vector3 position = new Vector3(i, j, 0.0f);
+                Collider[] otherObjectsInSpawnLocation = Physics.OverlapSphere(position, 0.25f);
+                bool positionIsUnoccupied = otherObjectsInSpawnLocation.Length == 0;
+                if (positionIsUnoccupied)
+                {
+                    availablePositions.Add(position);
+                }
+            }
+        }
+        return availablePositions;
     }
 
 }
